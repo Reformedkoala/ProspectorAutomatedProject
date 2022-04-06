9bbfba7dd84923c99e831de753e837807dbd589b
Reformedkoala <aightb22@gmail.com>
2022-04-06 14:39:17 -0600

    Initial Push to add pseudocode and workflow

---
+++ main.py
@@ -0,0 +1,7 @@
+# Pseudocode
+# Download file from email and place in directory with project (Look at interacting directly with outlook)
+# Open xlsx file and delete first three rows of data and check if last row has data not needed
+# Save edited file as both csv and xlsx and add the date in the format YYYY-MM-DD to the end of the name before the extension
+# Verify that all Unique Identifiers have 2341 as the last 4 digits of column 1 in the CSV file
+# Move new files to //LB/SharedSpace/Systems/Discovery/Prospector/Patron Records/
+# Delete the active file from the directory
