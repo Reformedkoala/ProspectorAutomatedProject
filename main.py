753262c559e29ed581daeb7c211fd0876dce6478
Reformedkoala <aightb22@gmail.com>
2022-04-08 14:44:30 -0600

    comments

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Pseudocode
-# Download file from email and place in directory with project (Look at interacting directly with outlook)
-# Open xlsx file and delete first three rows of data and check if last row has data not needed
+# Download file from email and place in directory with project (Look at interacting directly with outlook) - start interacting with the shared box
+# Open xlsx file and delete first three rows of data and check if last row has data not needed - done
 # Save edited file as both csv and xlsx and add the date in the format YYYY-MM-DD to the end of the name before the extension
 # Verify that all Unique Identifiers have 2341 as the last 4 digits of column 1 in the CSV file
 # Move new files to //LB/SharedSpace/Systems/Discovery/Prospector/Patron Records/
