68615ba1fe9ceeba9dae22bcf158b12cbd05ec6d
Reformedkoala <aightb22@gmail.com>
2022-04-08 14:08:35 -0600

    email is downloaded

--- main.py
+++ main.py
@@ -5,3 +5,27 @@
 # Verify that all Unique Identifiers have 2341 as the last 4 digits of column 1 in the CSV file
 # Move new files to //LB/SharedSpace/Systems/Discovery/Prospector/Patron Records/
 # Delete the active file from the directory
+
+import datetime
+import os
+import win32com.client
+path = os.path.expanduser("C:\\Users\\garrettthompson_a\\Downloads")
+today = datetime.date.today()
+outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+inbox = outlook.GetDefaultFolder(6)
+messages = inbox.Items
+messages.Sort("[ReceivedTime]", True)
+
+
+def save_attachments(subject, messages):
+    message = messages.GetFirst()
+    print(str(message))
+    if message.Subject == subject:
+        # body_content = message.body
+        attachments = message.Attachments
+        attachment = attachments.Item(1)
+        attachment.SaveAsFile(os.path.join(path, str(attachment)))
+        if message.Subject == subject and message.Unread:
+            message.Unread = False
+
+
