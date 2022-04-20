f981519905546e1b0d36c07299acade8526bc678
Reformedkoala <aightb22@gmail.com>
2022-04-20 13:14:00 -0600

    Commenting and refactoring code

--- ProspectorFunctions.py
+++ ProspectorFunctions.py
@@ -1,11 +1,4 @@
-# Pseudocode
-# Download file from email and place in directory with project (Look at interacting directly with outlook) - start interacting with the shared box
-# Open xlsx file and delete first three rows of data and check if last row has data not needed - done
-# Save edited file as both csv and xlsx and add the date in the format YYYY-MM-DD to the end of the name before the extension
-# Verify that all Unique Identifiers have 2341 as the last 4 digits of column 1 in the CSV file
-# Move new files to //LB/SharedSpace/Systems/Discovery/Prospector/Patron Records/
-# Delete the active file from the directory
-
+# all imports needed for the project
 import datetime
 import os
 import win32com.client
@@ -15,48 +8,56 @@ import csv
 import shutil
 
 
-# This function converts the csv data that we have cleaned into something that we can use a bit
-# easier and make it look like the data that we already have
+# This function converts the xlsx data into a csv per the requirement for the patron records folder
 def convert_xlsx_csv():
     read_file = pd.read_excel("ProspectorPatrons.xlsx")
     read_file.to_csv("ProspectorPatrons.csv", index=None, header=True)
 
 
-# This function allows us to automatically space the columns within the excel file so the user no longer has to do this
-# themselves this is done through the openpyxl library
+# This function allows us to delete all empty rows from the file as well in order to check that there are no empty
+# new lines
 def delete_rows(sheet):
     # iterate the sheet by rows
     for row in sheet.iter_rows():
-
         # all() return False if all of the row value is None
         if not all(cell.value for cell in row):
             # detele the empty row
             sheet.delete_rows(row[0].row, 1)
-
             # recursively call the remove() with modified sheet data
             delete_rows(sheet)
             return
 
 
+# This function allows us to save the attachment directly from the email as long as outlook is installed
+# and Libsys is a shared mailbox accessible on the computer. I am able to do this by starting a process of outlook
+# in the code and then iterating to the most recent email with the attachment we are looking for.
 def save_attachments(subject, messages):
+    # check variable to ensure we find the email and return an error if not
     check = 0
+    # for loop to iterate over all messages until we find the one with the correct attachment
     for message in messages:
+        # luckily the message we want will always have the same subject allowing me to set a constant variable for it
         if message.Subject == subject:
+            # using the win32 library and the objects and methods that come with it to save the attachment
             attachments = message.Attachments
             attachment = attachments.Item(2)
             attachment.SaveAsFile(os.path.join(path, str(attachment)))
+            # marking it as read if needed
             if message.Subject == subject and message.Unread:
                 message.Unread = False
+            # loading a workbook in order to delete the rows and not have to worry about empty rows in the data
             xfile = openpyxl.load_workbook("ProspectorPatrons.xlsx")
             sheet = xfile["Sheet1"]
             delete_rows(sheet)
             for row in sheet.iter_rows():
                 sheet.delete_rows(row[0].row, 1)
                 break
+            # saving the workbook to the proper xlsx file and converting it to a csv file
             xfile.save("ProspectorPatrons.xlsx")
             convert_xlsx_csv()
             check = 1
             break
+    # checking for errors if the email wasn't found
     if check == 1:
         print("Email found and converted successfully")
     else:
@@ -65,11 +66,13 @@ def save_attachments(subject, messages):
 
 
 def change_filename(filename1, filename2):
+    # using the shutil library to move the file onto the Y: drive as the os library does not allow access across drives
     shutil.move(filename1, xlsx_file)
     shutil.move(filename2, csv_file)
 
 
 def check_csv(csv_file):
+    # checking the csv file for any errors in the UIDS so that they all end in 2341
     with open(csv_file) as csvfile:
         fileread = csv.reader(csvfile)
         for row in fileread:
@@ -81,18 +84,25 @@ def check_csv(csv_file):
         print("File is correct")
 
 
+# setting our default path variable to originally save the xlsx file to in order to manipulate it within the program
 path = os.path.expanduser("C:\\Users\\garrettthompson_a\\Downloads\\Prospector\\ProspectorAutomatedProject")
-path2 = "Y:\\LB\\SharedSpace\\Systems\\Discovery\\Prospector\\" #Patron Records\\
+# path to where we need the files to be
+path2 = "Y:\\LB\\SharedSpace\\Systems\\Discovery\\Prospector\\"  # Patron Records\\
+# finding the date for when we save the file
 today = datetime.date.today()
+# opening the outlook application and finding the proper inbox
 outlook = win32com.client.Dispatch('outlook.application').GetNamespace("MAPI").Folders
 folder = outlook(1)
 inbox = folder.Folders("Inbox")
 messages = inbox.Items
+# sorting the messages because for some reason it doesn't always collect them in order of date and time
 messages.Sort("[ReceivedTime]", True)
 date = datetime.date.today().strftime('%Y-%m-%d')
+# changing the file names to what we actually need them to be
 xlsx_file = path2 + "minespatrons" + date + ".xlsx"
 csv_file = path2 + "minespatrons" + date + ".csv"
 
+# 3 function calls to actually perform everything
 save_attachments("ProspectorPatrons", messages)
 change_filename("ProspectorPatrons.xlsx", "ProspectorPatrons.csv")
 check_csv(csv_file)
